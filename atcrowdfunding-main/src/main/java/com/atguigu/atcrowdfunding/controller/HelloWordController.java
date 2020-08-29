8e45b1cebb99f2157a843cc4e65196fa7d7f6b84
jia1551189282 <1551189282@qq.com>
2020-08-29 22:27:05 +0800

    尚筹网-项目初始化

---
+++ atcrowdfunding-main/src/main/java/com/atguigu/atcrowdfunding/controller/HelloWordController.java
@@ -0,0 +1,12 @@
+package com.atguigu.atcrowdfunding.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author zjiajia
+ * @date 2020/8/29 22:20
+ */
+
+@Controller
+public class HelloWordController {
+}
